0282f2733a8f125b6d287aede1ca4d6b73595854
satyssat <couyssat.satya@gmail.com>
2021-09-01 18:17:46 +0200

    Formulaire

--- form-checker/index.js
+++ form-checker/index.js
@@ -2,45 +2,153 @@ const inputs = document.querySelectorAll(
   `input[type="text"], input[type="password"]`
 );
 
+let pseudo, email, password, confirmPass;
+const progressBar = document.getElementById("progress-bar");
+
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------                     FUNCTION errorDisplay
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+
 const errorDisplay = (tag, message, valid) => {
-  const container = document.querySelectorAll("." + tag + "-container");
-  const span = document.querySelector("." + tag + "-container" > span);
+  const container = document.querySelector("." + tag + "-container");
+  const span = document.querySelector("." + tag + "-container > span");
 
   if (!valid) {
     container.classList.add("error");
     span.textContent = message;
+  } else {
+    container.classList.remove("error");
+    span.textContent = message;
   }
 };
-
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------                     PSEUDO
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
 const pseudoChecker = (value) => {
   if (value.length > 0 && (value.length < 3 || value.length > 20)) {
     errorDisplay("pseudo", "Le pseudo doit contenir entre 3 et 20 caractères");
+    pseudo = null;
   } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
     errorDisplay(
       "pseudo",
       "Le pseudo ne doit pas contenir de caractères spéciaux"
     );
+    pseudo = null;
   } else {
     errorDisplay("pseudo", "", true);
+    pseudo = value;
   }
 };
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------                     EMAIL
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
 const mailChecker = (value) => {
-  console.log(value);
+  if (!value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    errorDisplay("email", "Le mail n'est pas valide");
+    email = null;
+  } else {
+    errorDisplay("email", "", true);
+    email = value;
+  }
 };
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------                     PASSWORD
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
 const passwordChecker = (value) => {
-  console.log(value);
+  progressBar.classList = "";
+  if (!value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
+    errorDisplay(
+      "password",
+      "Longueur minimum : 8 dont 1 majuscule, 1 chiffre et 1 minuscule"
+    );
+    password = null;
+    progressBar.classList.add("progressRed");
+  } else if (value.length < 12) {
+    progressBar.classList.add("progressBlue");
+    errorDisplay("password", "", true);
+    password = value;
+  } else {
+    progressBar.classList.add("progressGreen");
+    errorDisplay("password", "", true);
+    password = value;
+  }
 };
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------                     CONFIRMPASSWORD
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
 const confirmChecker = (value) => {
-  console.log(value);
+  if (value !== password) {
+    console.log("marche pas");
+    errorDisplay("confirm", "Les mots de passe ne correspondent pas");
+  } else {
+    console.log("Bingo !!");
+    errorDisplay("confirm", "", true);
+  }
 };
-
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------                     FOREACH INPUT
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
+//---------------
 inputs.forEach((input) => {
   input.addEventListener("input", (e) => {
     switch (e.target.id) {
       case "pseudo":
         pseudoChecker(e.target.value);
         break;
-      case "mail":
+      case "email":
         mailChecker(e.target.value);
         break;
       case "password":
